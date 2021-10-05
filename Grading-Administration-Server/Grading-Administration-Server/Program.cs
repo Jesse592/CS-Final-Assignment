611d2d70cf37f9fb36a58a076835fe59f265aa7e
Jesse <jm.krijgsman@student.avans.nl>
2021-10-05 14:14:15 +0200

    Created server project

---
+++ Grading-Administration-Server/Grading-Administration-Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Grading_Administration_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
