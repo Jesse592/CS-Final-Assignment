ebc97eda9a9d64ab4f7e486e7e7e19c4e4f76744
Twan van Noorloos <twan.v.n@outlook.com>
2021-10-26 21:22:46 +0200

    Added databinding to add user in admin

--- Grading-Administration/Gradings-Administration-client/ViewModels/AdminViewModel.cs
+++ Grading-Administration/Gradings-Administration-client/ViewModels/AdminViewModel.cs
@@ -1,16 +1,30 @@
-﻿using Gradings_Administration_client.ViewModels;
+﻿
+
+using Grading_Administration_Server.EntityFramework.models;
+using Grading_Administraton_Shared.Entities;
+using GradingAdmin_client.Handlers;
+using Gradings_Administration_client.ViewModels;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
 
 namespace GradingAdmin_client.ViewModels
 {
-    class AdminViewModel : BaseViewModel
+    class AdminViewModel : BaseViewModel, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private AdminHandlerVM handler;
+
         public AdminViewModel()
         {
+            this.handler = new AdminHandlerVM();
+
+            this.userTypes = new List<string>();
+            this.userTypes.Add(UserType.STUDENT.ToString());
+            this.userTypes.Add(UserType.TEACHER.ToString());
+            this.userTypes.Add(UserType.ADMIN.ToString());
         }
 
         private string _FirstName;
@@ -53,13 +67,61 @@ namespace GradingAdmin_client.ViewModels
             }
         }
 
-        private int _UserType;
-        public int UserType
+        private List<string> _userTypes;
+        public List<string> userTypes
         {
-            get { return _UserType; }
+            get { return _userTypes; }
             set
             {
-                _UserType = value;
+                _userTypes = value;
+            }
+        }
+
+        private int _userType;
+        public int userType
+        {
+            get { return _userType; }
+            set
+            {
+                _userType = value;
+            }
+        }
+
+        private DateTime _CurrentDate;
+        public DateTime CurrentDate
+        {
+            get { return _CurrentDate; }
+            set
+            {
+                _CurrentDate = value;
+                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CurrentDate"));
+            }
+        }
+
+        private ICommand _AddUser;
+        public ICommand AddUser
+        {
+            get
+            {
+                if (_AddUser == null)
+                {
+                    _AddUser = new GeneralCommand(
+                        param => SendUser()
+                        ); ;
+                }
+                return _AddUser;
+            }
+
+        }
+
+        private void SendUser()
+        {
+            if (FirstName == "" || LastName == "" || Mail == "")
+            {
+
+            } else
+            {
+                User u = new User(0, FirstName, LastName, BirthDate, Mail, userType + "");
             }
         }
     }
