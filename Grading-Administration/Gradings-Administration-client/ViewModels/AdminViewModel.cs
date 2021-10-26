a0d5f69207885dad8fbd24ac5ddfafead7cc2654
Twan van Noorloos <twan.v.n@outlook.com>
2021-10-26 20:59:26 +0200

    Implemented admin GUI

--- Grading-Administration/Gradings-Administration-client/ViewModels/AdminViewModel.cs
+++ Grading-Administration/Gradings-Administration-client/ViewModels/AdminViewModel.cs
@@ -9,5 +9,58 @@ namespace GradingAdmin_client.ViewModels
 {
     class AdminViewModel : BaseViewModel
     {
+        public AdminViewModel()
+        {
+        }
+
+        private string _FirstName;
+        public string FirstName
+        {
+            get { return _FirstName; }
+            set
+            {
+                _FirstName = value;
+            }
+        }
+
+        private string _LastName;
+        public string LastName
+        {
+            get { return _LastName; }
+            set
+            {
+                _LastName = value;
+            }
+        }
+
+        private string _Mail;
+        public string Mail
+        {
+            get { return _Mail; }
+            set
+            {
+                _Mail = value;
+            }
+        }
+
+        private DateTime _Birthdate;
+        public DateTime BirthDate
+        {
+            get { return _Birthdate; }
+            set
+            {
+                _Birthdate = value;
+            }
+        }
+
+        private int _UserType;
+        public int UserType
+        {
+            get { return _UserType; }
+            set
+            {
+                _UserType = value;
+            }
+        }
     }
 }
