32716cd0bce11e47639546f76aab58aebaf6769f
Jesse <jm.krijgsman@student.avans.nl>
2021-10-28 11:25:35 +0200

    Created first unittest, test tryparse

---
+++ Grading-Administration/Test-Grading-Administration-Server/Helper/JSONUtilTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Grading_Administration_Shared.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Grading_Administration_Shared.Util.Tests
+{
+    [TestClass]
+    public class JSONUtilTests
+    {
+        /// <summary>
+        /// Testing the TryParse extension method with a correct string
+        /// </summary>
+        [TestMethod]
+        public void TryParseTestSucces()
+        {
+            // Arrange
+            JObject jObject = new JObject();
+            string testString = "{\"command\": \"login\",\"data\": {\"username\": \"jesse\",\"password\": \"123\"}}";
+
+            // Act
+            JObject resultObject;
+
+            bool canParse = jObject.TryParse(testString, out resultObject);
+
+            // Assert
+            Assert.IsTrue(canParse);
+            Assert.IsNotNull(resultObject);
+        }
+    }
+}
---
+++ Grading-Administration/Test-Grading-Administration-Server/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_Grading_Administration_Server
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
